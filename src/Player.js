cbb8309dc034ceb391302050e80c1c2a2b01a2ee
magnus <magnus@iastate.edu>
2021-12-15 06:23:51 -0600

    Fix fullscreen mode for theater mode - still won't automatically enter theater mode

--- src/Player.js
+++ src/Player.js
@@ -1,25 +1,26 @@
-console.log("Testy tester testing test");
-
 // Make the theater container use the full screen
-waitForElm("player-theater-container").then((elm) => {
-    document.getElementById("player-theater-container").style.maxHeight = "calc(100vh - 56px)";
-});
+waitForElm("player-theater-container").then(correctTheaterMode);
+document.addEventListener("fullscreenchange", correctTheaterMode);
+// Would be a better solution I think, because it doesn't involve constantly peering around and modifying the DOM, but it doesn't function.
+// console.log("starting");
+// browser.tabs.insertCSS({code:THEATER_CSS, runAt: "document_start"}).then(() => {console.log("inserted");}, onerror);
 
 // Make the description automatically expanded
-waitForElm("description").then((elm) => {
-	document.getElementById("description").parentElement.remove();
-});
+// waitForElm("description").then((elm) => {
+// 	document.getElementById("description").parentElement.remove();
+// });
 
-// unset #primary.ytd-watch-flexy-max-player-width
-// Unset all of #primary 's styling
-// unset #player.ytd-watch-flexy-max-player-width
+const THEATER_SIZE = "calc(100vh - 56px)";
+const FULLSCREEN_SIZE = "100vh";
+function correctTheaterMode() {
+	document.getElementById("player-theater-container").style.maxHeight =
+		document.fullscreenElement ? FULLSCREEN_SIZE : THEATER_SIZE;
+}
 
 // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists#16726669
 function waitForElm(selector) {
 	return new Promise((resolve) => {
 		let elm = document.getElementById(selector);
-
-        console.log(elm);
 
 		if (elm) return resolve(elm);
 
@@ -31,7 +32,6 @@ function waitForElm(selector) {
 				observer.disconnect();
 			}
 		});
-
 
 		observer.observe(document.body, {
 			childList: true,
