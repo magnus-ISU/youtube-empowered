9f4d9578d4f772765b0b021bd1f9c5081878b800
magnus <magnus@iastate.edu>
2021-12-19 04:07:06 -0600

    Refactor code to use CSS rather than javascript, and add floating player

--- src/Player.js
+++ src/Player.js
@@ -1,19 +1,37 @@
-// Make the theater container use the full screen
-waitForElm("player-theater-container").then(correctTheaterMode);
-document.addEventListener("fullscreenchange", correctTheaterMode);
-// Would be a better solution I think, because it doesn't involve constantly peering around and modifying the DOM, but it doesn't function.
-// console.log("starting");
-// browser.tabs.insertCSS({code:THEATER_CSS, runAt: "document_start"}).then(() => {console.log("inserted");}, onerror);
+////////////////////// Make the theater container use the full screen /////////////////////////
+// Just a stylesheet
+browser.runtime.sendMessage({ a: "css", css: "/src/BigTheater.css" });
 
+////////////////////// Make the window move to a popup if scrolled down ///////////////////////
+// Observe when the video player moves out of screen and apply a class to the document's body when it does. FloatingPlayer has a rule to show popup when document has that class
+browser.runtime.sendMessage({ a: "css", css: "/src/FloatingPlayer.css" });
 
-const THEATER_SIZE = "calc(100vh - 56px)";
-const FULLSCREEN_SIZE = "100vh";
-function correctTheaterMode() {
-	document.getElementById("player-theater-container").style.maxHeight =
-		document.fullscreenElement ? FULLSCREEN_SIZE : THEATER_SIZE;
+const options = {
+	root: null,
+	threshold: 0.3,
+};
+const observer = new IntersectionObserver(addPopupWhenLeavingViewport, options);
+// ytd-player is the main video player. It is not initially loaded
+waitForElm("ytd-player").then(createPopupObserver);
+
+function createPopupObserver() {
+	observer.observe(document.getElementById("ytd-player"));
+}
+
+// https://css-tricks.com/styling-based-on-scroll-position/
+function addPopupWhenLeavingViewport(entries) {
+	console.log(entries);
+	if (entries[0].intersectionRatio < options.threshold) {
+		document.body.classList.add("YTempowered-popup");
+	} else {
+		document.body.classList.remove("YTempowered-popup");
+		// We must trigger a resize event as youtube itself hardcodes video size into the <video> element
+		window.dispatchEvent(new Event('resize'));
+	}
 }
 
 // https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists#16726669
+// Waits until an element is loaded, since youtube loads stuff in weirdly
 function waitForElm(selector) {
 	return new Promise((resolve) => {
 		let elm = document.getElementById(selector);
